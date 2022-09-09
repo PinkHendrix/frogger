503f6164565daacb71b95503e21eba6ca02c799d
Rick Hausz <pinkhendrix1881@gmail.com>
2022-09-08 17:09:14 -0700

    Got top row of logs moving, yay!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const StartPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+const logsleft = document.querySelectorAll('.log-left');
 
 console.log(squares);
 
@@ -36,3 +37,34 @@ function moveFrog(e) {
   squares[currentIndex].classList.add('frog');
 }
 document.addEventListener('keyup', moveFrog);
+
+function autoMoveLogs() {
+  logsleft.forEach((logLeft) => moveLogLeft(logLeft));
+}
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains('l1'):
+      logLeft.classList.remove('l1');
+      logLeft.classList.add('l2');
+      break;
+    case logLeft.classList.contains('l2'):
+      logLeft.classList.remove('l2');
+      logLeft.classList.add('l3');
+      break;
+    case logLeft.classList.contains('l3'):
+      logLeft.classList.remove('l3');
+      logLeft.classList.add('l4');
+      break;
+    case logLeft.classList.contains('l4'):
+      logLeft.classList.remove('l4');
+      logLeft.classList.add('l5');
+      break;
+    case logLeft.classList.contains('l5'):
+      logLeft.classList.remove('l5');
+      logLeft.classList.add('l1');
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 1000);
